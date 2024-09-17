3e261d8369071d9e487237363e2756efb5b1c841
igorekmniejniz3 <igor.wojcik@tm1.edu.pl>
2024-09-17 17:53:16 +0200

    strona glowna dodanie navbar i podglad skina

--- ojtko-tris/src/App.jsx
+++ ojtko-tris/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav className="navbar">
+        <div className="container-fluid">
+          <div className="d-flex flex-grow-1">
+            <div className="mx-auto">
+              <span className="navbar-text">#Ojtko2115</span>
+            </div>
+            <div className="ml-auto">
+              <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68" viewBox="0 0 24 24">
+                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z" fill="white"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 left-side">
+            <div className="character-square"></div>
+            <div className="skin-selector">
+              <div className="skin-thumbnail">Skin 1</div>
+              <div className="skin-thumbnail">Skin 2</div>
+              <div className="skin-thumbnail">Skin 3</div>
+              <div className="skin-thumbnail">Skin 4</div>
+              <div className="skin-thumbnail">Skin 5</div>
+              <div className="skin-thumbnail">Skin 6</div>
+              <div className="skin-thumbnail">Skin 7</div>
+              <div className="skin-thumbnail">Skin 8</div>
+              <div className="skin-thumbnail">Skin 9</div>
+
+            </div>
+          </div>
+          <div className="col-md-6 right-side">
+            <h2>Wybór trybu gry</h2>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
