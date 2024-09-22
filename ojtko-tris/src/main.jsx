2dd2dfd0d0280da1636d2e3dab5b68f9462f1d20
igorekmniejniz3 <igor.wojcik@tm1.edu.pl>
2024-09-22 16:16:52 +0200

    dodanie panelu logowania i lekka zmiana w home

--- ojtko-tris/src/main.jsx
+++ ojtko-tris/src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './Home.jsx'
+import Home from './sites/Home.jsx'
+import Login from './sites/Login.jsx'
+import Register from './sites/Register.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// import './index.css'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/>
       </Routes>
     </Router>
   </StrictMode>,
---
+++ ojtko-tris/src/sites/Register.jsx
@@ -0,0 +1,31 @@
+import './../styles/Login.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <div className="d-flex vh-100 justify-content-center align-items-center">
+        <div className="square-div">
+          <div className="title mb-4">Zaloguj się</div>
+          <form className="mb-3">
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input type="email" id="email" className="form-control" placeholder="Wprowadź email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Hasło</label>
+              <input type="password" id="password" className="form-control" placeholder="Wprowadź hasło" />
+            </div>
+            <button type="submit" className="btn btn-dark">Zaloguj</button>
+          </form>
+          <Link to="/register">
+            <button type="button" className="btn btn-link register">Utwórz konto</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
