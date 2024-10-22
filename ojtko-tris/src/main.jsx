3dd7b5040750299e0f7e3941d7722a50273a6b7c
Daxmil <105710529+Daxmil@users.noreply.github.com>
2024-10-22 23:34:32 +0200

    reklama

--- ojtko-tris/src/main.jsx
+++ ojtko-tris/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./sites/Home.jsx";
 import Login from "./sites/Login.jsx";
@@ -10,18 +10,42 @@ import User from "./sites/User.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 
+const App = () => {
+  const [isAudioLoaded, setIsAudioLoaded] = useState(false);
+
+  useEffect(() => {
+    const audio = new Audio("src")
+
+    const playAudio = async () => {
+      await audio.play();
+      setIsAudioLoaded(true);
+    };
+
+    playAudio();
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="mega" element={<Mega />} />
+      <Route path="timer" element={<Timer />} />
+      <Route path="normal" element={<Normal />} />
+      <Route path="user" element={<User />} />
+    </Routes>
+  );
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="mega" element={<Mega />} />
-        <Route path="timer" element={<Timer />} />
-        <Route path="normal" element={<Normal />} />
-        <Route path="user" element={<User />} />
-      </Routes>
+      <App />
     </Router>
   </StrictMode>
 );
